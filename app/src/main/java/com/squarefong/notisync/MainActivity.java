dc73a87d9d0833b400ef6db0277eb11641a9f5e3
SquareFong <squarefong17@gmail.com>
2020-03-27 23:58:39 +0800

    add menu activity

--- app/src/main/java/com/squarefong/notisync/MainActivity.java
+++ app/src/main/java/com/squarefong/notisync/MainActivity.java
@@ -3,6 +3,7 @@ package com.squarefong.notisync;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option, menu);
+        return true;
     }
 }
