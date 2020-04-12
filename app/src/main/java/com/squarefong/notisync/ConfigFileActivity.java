94f0edb166b2e9ba7e0d8e32ab9527075a345c25
SquareFong <fanghaocheng2008@126.com>
2020-04-13 03:23:56 +0800

    Improve the UI of Config file activity

--- app/src/main/java/com/squarefong/notisync/ConfigFileActivity.java
+++ app/src/main/java/com/squarefong/notisync/ConfigFileActivity.java
@@ -2,7 +2,12 @@ package com.squarefong.notisync;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ConfigFileActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class ConfigFileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_file);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_config_file, menu);
+        return true;
+    }
+
+    @SuppressLint("ShowToast")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_config:
+                Toast.makeText(ConfigFileActivity.this, "You Click Config File",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.del_config:
+                Toast.makeText(ConfigFileActivity.this, "You Click Delete File",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
