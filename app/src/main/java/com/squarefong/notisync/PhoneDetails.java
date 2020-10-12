576966b53029c160a84231e01ff33a764e9701c0
Square Fong <squarefong17@gmail.com>
2020-10-13 02:31:47 +0800

    add MessageItem

--- app/src/main/java/com/squarefong/notisync/PhoneDetails.java
+++ app/src/main/java/com/squarefong/notisync/PhoneDetails.java
@@ -2,3 +2,25 @@ package com.squarefong.notisync;
 
 public class PhoneDetails {
 }
+
+
+class DetailItem{
+    String OsVersion;
+    String Model;
+    String Kernel;
+    String Uptime;
+    String Processor;
+    String MemoryUsage;
+    String StorageUsage;
+    public DetailItem(String osVersion, String model, String kernel,
+                      String uptime, String processor,
+                      String memoryUsage, String storageUsage){
+        OsVersion = osVersion;
+        Model = model;
+        Kernel = kernel;
+        Uptime = uptime;
+        Processor = processor;
+        MemoryUsage = memoryUsage;
+        StorageUsage = storageUsage;
+    }
+}
